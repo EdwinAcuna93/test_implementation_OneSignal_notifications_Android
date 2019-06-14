2ead3c359e669b5ce4579f3cafc1465906d97f25
Edwin Acuna <edwinalex0093@gmail.com>
2019-06-14 12:11:34 -0600

    commit inicial implementacion de clase para recibir notificaciones pushs desde oneSignal

---
+++ app/src/main/java/com/example/androidfcm/notificaciones.java
@@ -0,0 +1,18 @@
+package com.example.androidfcm;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+
+public class notificaciones extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
